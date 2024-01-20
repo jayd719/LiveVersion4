c2ad1f4620073ef5abd0fd931751bc8abcf829a4
JD-MBP <jashansingh29@gmail.com>
2024-01-20 12:20:20 -0500

    ;

--- workOrderReports/views.py
+++ workOrderReports/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+#19301-01
+def isWorkOrderValid(wo):
+    isValid = False
+    if len(wo)==8 and wo[0:5].isnumeric() and wo[5]=='-' and wo[6:].isnumeric():
+        isValid=True
+    return isValid
 
 
-def check_work_order_number():
-    pass
 
+@login_required
 def workOrderReport(requests):
+    data={}
     
-    workOrder = requests.GET.get('workOrderBar')
-    
 
-
-
-    data = {'title':workOrder}
+    if 'search-for-work-order' in requests.GET:
+        workOrder = requests.GET.get('search-for-work-order')
+        
+        if(isWorkOrderValid(workOrder)):
+            data = {'title':workOrder,
+                    'workOrder':workOrder}
+        else:
+            data ={'message':f'{workOrder} is Not a Valid Work Order Number',}
 
     return render(requests, 'workOrderReports/report.html', context=data)
 
