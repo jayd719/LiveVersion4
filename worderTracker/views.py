acf2234dc910e231a738ad195adff32f97880d96
JD-MBP <jashansingh29@gmail.com>
2024-01-27 13:17:09 -0500

    ;

--- worderTracker/views.py
+++ worderTracker/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from workOrderReports.views import workOrders
 
 def monthly_forcast(requests):
-    return render(requests,'tracker/tracker.html',{'title':'cn'})
+    return render(requests,'tracker/tracker.html',{'title':'Live','sList':workOrders})
