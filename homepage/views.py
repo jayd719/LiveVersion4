dbbf5cf7fcd0c9d172734319631bde52dac3d8c0
JD jd <jd@cbb.net>
2024-01-30 09:55:28 -0500

    ';'

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib import messages
 from workOrderReports.views import workOrders
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def home(requests):
     messages.success(requests,f'Signed In')
     return render(requests,'hp/home.html',{'sList':workOrders})
