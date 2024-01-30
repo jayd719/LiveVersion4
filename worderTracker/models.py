a3ccf570dfa6f734788ee2de464bb549fd81fe43
JD-MBP <jashansingh29@gmail.com>
2024-01-30 15:38:33 -0500

    ;

--- worderTracker/models.py
+++ worderTracker/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class WorkOrderTracker(models.Model):
     jobNumber =models.CharField(max_length=8,primary_key = True)
     dueDate = models.DateField()
-    incomingInspection = models.BooleanField(blank=True)
-    ME = models.CharField(blank=True,max_length=2)
-    rush = models.BooleanField(blank=True,)
-    notes1 = models.TextField(blank=True)
-    notes2 = models.TextField(blank=True)
+    incomingInspection = models.BooleanField(blank=True,null=True)
+    ME = models.CharField(blank=True,max_length=2,null=True)
+    rush = models.BooleanField(blank=True,null=True)
+    notes1 = models.TextField(blank=True,null=True)
+    notes2 = models.TextField(blank=True,null=True)
 
