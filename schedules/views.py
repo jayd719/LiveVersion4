8427f23877d12c32907dae74770430cf09ef721a
JD-MBP <jashansingh29@gmail.com>
2024-04-08 23:09:17 -0400

    ;

--- schedules/views.py
+++ schedules/views.py
@@ -1,14 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,HttpResponse
+from workOrderReports.views import workOrders
 from worderTracker.models import Operation
 from homepage.functions import userInfo
-from django.contrib import messages
-from django.http import JsonResponse
-from django.views.static import serve
 from .functions import createExcelSheet
-from workOrderReports.views import workOrders
+from django.http import JsonResponse
 import json
-import os
 
+@login_required
 def shd_home(requests):
     workCenters=Operation.objects.values_list('workCenter', flat=True).order_by('workCenter').distinct()
 
@@ -30,12 +29,12 @@ def shd_home(requests):
 
 
 
-
+@login_required
 def post_shd(requests):
     workC = requests.GET.get('shd-for-workCenter') 
     return render(requests,'schedules/shd-jobs.html',workC)
 
-
+@login_required
 def download_shd(requests):
     if requests.method == 'POST':
         try:
