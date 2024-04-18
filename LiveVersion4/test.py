c8931efa4f60d515217d1a3dd5440b22bc88b0d4
JD jd <jd@cbb.net>
2024-04-18 07:40:55 -0400

    ';

--- LiveVersion4/test.py
+++ LiveVersion4/test.py
@@ -5,12 +5,15 @@ from requests import get
 
 def getListofAllOrders():
     print('Generating Master List')
-    headers = {'accept': 'application/json', 'Authorization': f'Bearer {TOKEN()}'}
-    r = get('https://api-jb2.integrations.ecimanufacturing.com:443/api/v1/order-line-items?fields=jobNumber&sort=-dueDate&status=Open',headers=headers).json()['Data']
-    number_list = []
-    for wo in r:
-        if wo['jobNumber'] is not None:
-            number_list.append(wo['jobNumber'])
+    try:
+        headers = {'accept': 'application/json', 'Authorization': f'Bearer {TOKEN()}'}
+        r = get('https://api-jb2.integrations.ecimanufacturing.com:443/api/v1/order-line-items?fields=jobNumber&sort=-dueDate&status=Open',headers=headers).json()['Data']
+        number_list = []
+        for wo in r:
+            if wo['jobNumber'] is not None:
+                number_list.append(wo['jobNumber'])
+    except:
+        number_list=['workOrder1','WorkOrder2','WorkOrder3','WorkOrder4','WorkOrder5','WorkOrder6','WorkOrder7','WorkOrder8','WorkOrder9']
     return sorted(number_list)
 
 
